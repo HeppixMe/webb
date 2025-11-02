841d82172790d8c9f4c855922b38f51fbae0f41b
HeppixMe <Leopold.grassl@icloud.com>
2025-11-02 22:02:53 +0100

    C users path deleted

--- Projects/Cmd Rendering/test.py
+++ Projects/Cmd Rendering/test.py
@@ -3,5 +3,5 @@ w, h = 900, 500
 canvas = cmd.canvas(w,h)
 canvas.clear()
 canvas.drawBorder()
-canvas.render("C:/Users/leopold/Documents/cmd rendering/monkey.obj",200, 450,250, rotate=False,shading=True, color="white")
+canvas.render("monkey.obj",200, 450,250, rotate=False,shading=True, color="white")
 canvas.cursorResetBottom()
